022adde3b3774ca06a0e9237ff213e7f4e6ad12a
timvisee <timvisee@gmail.com>
2017-02-18 02:27:02 +0100

    Add documentation to BotState

--- src/main/java/com/timvisee/lumberbot/BotState.java
+++ src/main/java/com/timvisee/lumberbot/BotState.java
@@ -2,10 +2,33 @@ package com.timvisee.lumberbot;
 
 public enum BotState {
 
+    /**
+     * State used for initialization.
+     */
     INIT,
+
+    /**
+     * State the user must select the tree in.
+     */
     SELECT_TREE,
+
+    /**
+     * State the user must select the bottom branch in.
+     */
     SELECT_BRANCH,
+
+    /**
+     * State letting the user focus the game window. Before the bot starts playing.
+     */
     BEFORE_PLAYING,
+
+    /**
+     * State the bot is playing in.
+     */
     PLAYING,
+
+    /**
+     * Final state, where the bot is finished.
+     */
     DONE;
 }
